1e003528bc0011724beeb73eaee6dff96cad0ec3
Kim youngOk <kyo2685@didimu.co.kr>
2023-07-06 15:37:46 +0900

    kim_v3

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <Header></Header>
       <Content></Content>
-      <Footer>하단입니다.222 옆자리에서 수정</Footer>
+      <Footer>하단입니다.222 옆자리에서 수정 -  나도 수정했습니다.</Footer>
        
     </div>
   );
